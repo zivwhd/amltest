58a198122f3c0d43a2e88c513fde33b43b771a05
yonatanto <yonatanrtt@gmail.com>
2024-04-03 00:17:39 +0300

    remove test time

--- time_test/bs_20/sst.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-sys.path.append("../..")
-from config.tasks import SST_TASK
-from time_test.run import TestTime
-
-TestTime(task = SST_TASK, beam_size = 20).run()
--- time_test/bs_50/imdb.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-sys.path.append("../..")
-from config.tasks import IMDB_TASK
-from time_test.run import TestTime
-
-TestTime(task = IMDB_TASK, beam_size = 50).run()
